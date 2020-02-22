000ea4091498cbe4a4b2960be6f9319535848d20
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-02-22 15:20:15 +0800

    Create 30 最佳买卖股票时机含冷冻期 递归实现 超时.cpp

---
+++ 30 最佳买卖股票时机含冷冻期 递归实现 超时.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+	int max = INT_MIN;
+	vector<int> prices;
+
+	int maxProfit(vector<int>& prices) {
+		this->prices = prices;
+		dfs(false, -1, 0, 0);
+		return max;
+	}
+	void dfs(bool buyed, int buyprice, int tolprice, int index){
+  //结束条件
+		if (index >= prices.size()){
+			if (tolprice > max)
+				max = tolprice;
+			return;
+		}
+		//保持
+		dfs(buyed, buyprice, tolprice, index + 1);
+    //卖出
+		if (buyed){
+			if (prices[index] > buyprice){
+				tolprice += prices[index] - buyprice;
+				dfs(false, buyprice, tolprice, index + 2);
+			}	
+		}
+    //买入
+		else if (!buyed){
+			dfs(true, prices[index], tolprice, index + 1);
+		}
+	}
+};
