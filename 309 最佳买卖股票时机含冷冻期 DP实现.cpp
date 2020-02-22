63bbe65d80778d979564240e92ec7ccd888e481f
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-02-22 16:31:09 +0800

    Update and rename 309 最佳买卖股票时机含冷冻期.cpp to 309 最佳买卖股票时机含冷冻期 DP实现.cpp

--- 309 最佳买卖股票时机含冷冻期 DP实现.cpp
+++ 309 最佳买卖股票时机含冷冻期 DP实现.cpp
@@ -8,13 +8,12 @@ public:
 		hold[1] = max(hold[0], -prices[1]);
 		unhold[0] = 0;
 		unhold[1] = max(unhold[0], hold[0] + prices[1]);
-        int maxVal=max(hold[1],unhold[1]);
+        	int maxVal=max(hold[1],unhold[1]);
 		for (int i = 2; i < prices.size(); i++){
 			hold[i] = max(hold[i - 1], unhold[i - 2] - prices[i]);
 			unhold[i] = max(unhold[i - 1], hold[i - 1] + prices[i]);
 			maxVal = max(max(hold[i], unhold[i]), maxVal);
 		}
-
 		return maxVal;
 	}
 
