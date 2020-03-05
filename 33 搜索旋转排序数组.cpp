e629826edd4eab6f84de185187ef3559d99a71dc
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-03-05 12:13:07 +0800

    Create 33 搜索旋转排序数组.cpp

---
+++ 33 搜索旋转排序数组.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		int left = 0, right = nums.size() - 1;
+		int mid;
+		while (left <= right){
+			mid = (right + left) / 2;
+			if (nums[mid] == target)
+				return mid;
+			if (nums[mid] > nums[right]){
+				if (target >= nums[left] && target<nums[mid])
+					right = mid - 1;
+				else
+					left = mid + 1;
+			}
+			else if (nums[mid] < nums[left]){
+				if (target <= nums[right] && target>nums[mid])
+					left = mid + 1;
+				else
+					right = mid - 1;
+			}
+			else{
+				if (nums[mid] > target)
+					right = mid - 1;
+				else
+					left = mid + 1;
+			}
+		}
+		return -1;
+	}
+};
