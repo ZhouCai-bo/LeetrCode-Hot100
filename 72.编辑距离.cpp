5e52fa0381b53aaf0fda27af26ee6342bd8e650a
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-03-11 10:07:39 +0800

    Create 72.编辑距离.cpp

---
+++ 72.编辑距离.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	int minDistance(string word1, string word2) {
+		vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1));
+		dp[0][0] = 0;
+		for (int i = 1; i <= word1.size(); i++)
+			dp[i][0] = dp[i - 1][0] + 1;
+		for (int i = 1; i <= word2.size(); i++)
+			dp[0][i] = dp[0][i - 1] + 1;
+		for (int i = 1; i <= word1.size(); i++)
+		{
+			for (int j = 1; j <= word2.size(); j++)
+			{
+				if (word1[i - 1] == word2[j - 1])
+					dp[i][j] = dp[i - 1][j - 1];
+				else
+					dp[i][j] = min(min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1;
+			}
+		}
+		return dp[word1.size()][word2.size()];
+	}
+};
