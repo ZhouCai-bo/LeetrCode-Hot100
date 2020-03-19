39d5119340426b55b466585c445ad1940eed11f1
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-03-19 10:40:52 +0800

    Create 96 不同的二叉搜索树.cpp

---
+++ 96 不同的二叉搜索树.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> res(n+1);
+        res[0]=res[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                res[i]+=res[j]*res[i-j-1];
+            }
+        }
+        return res[n];
+    }
+};
