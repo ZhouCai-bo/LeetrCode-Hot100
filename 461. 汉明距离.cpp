b66e191e0cf78d30fc861bc65f0cbb51e342cd4e
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-04-20 11:49:33 +0800

    Create 461. 汉明距离.cpp

---
+++ 461. 汉明距离.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int hammingDistance(int x, int y) {
+		int cnt = 0;
+		while (x > 0 || y > 0)
+		{
+			cnt += (x & 1) ^ (y & 1);
+			x /= 2;
+			y /= 2;
+		}
+		return cnt;
+	}
+};
