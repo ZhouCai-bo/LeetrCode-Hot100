ca82851b12c6e9ec72de4ee28defbadead9eb0ab
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-04-24 10:43:08 +0800

    Create 739. 每日温度.cpp

---
+++ 739. 每日温度.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+	vector<int> dailyTemperatures(vector<int>& T) {
+		stack<int> st;
+		vector<int> res(T.size());
+
+		for (int i = 0; i < T.size(); i++)
+		{
+			while (!st.empty() && T[st.top()] < T[i])
+			{
+				res[st.top()] = i - st.top();
+				st.pop();
+			}
+			st.push(i);
+		}
+		while (!st.empty())
+		{
+			res[st.top()] = 0;
+			st.pop();
+		}
+		return res;
+	}
+};
